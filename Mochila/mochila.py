325b80e4b93f51fb3cb35a549c0c9a160435e315
jjmv <jjmv.97@hotmail.com>
2017-02-10 15:19:07 -0600

    Se han añadido los 8 casos extraidos de www.people.sc.fsu.edu proporcionados por la encargada del curso mas el ejemplo propuesto en clase

--- Mochila/mochila.py
+++ Mochila/mochila.py
@@ -1,7 +1,9 @@
 import sys
 import csv
+import time
 
-print("Ingrese el nombre del archivo [Clase], [TareaSinCeros] [TareaConCeros]")
+start_time = time.clock()
+print("\nIngrese el nombre del archivo [Clase],[Caso1],[Caso2]....[Caso'n']")
 nombre = input() + ".csv"
 valores_objetos = []
 try:
@@ -34,7 +36,7 @@ promedio = 0
 for i in range(len(objetos)):
     promedio += objetos[i][1]
 promedio = promedio / (len(objetos))
-
+print("La capacidad máxima es: " + str(capacidad))
 lista_no_excede = []
 lista_excede_promedio= []
 for i in range(len(objetos)):
@@ -44,7 +46,6 @@ for i in range(len(objetos)):
     else:
         valor = objetos[i]
         lista_excede_promedio.append(valor)
-
 lista_reordenada = []
 taken = []
 rango = len(lista_no_excede)
@@ -57,7 +58,6 @@ while(rango != 0):
                 indicador = j
                 mayor = lista_no_excede[j][2]
                 valor = lista_no_excede[j]
-
     taken.append(indicador)
     lista_reordenada.append(valor)
     rango -= 1
@@ -71,13 +71,13 @@ while(rango != 0):
             if not(w in taken2):
                 menor = lista_excede_promedio[w][1]
                 valor = lista_excede_promedio[w]
-            break
+                indicador = w
+
         for j in range(len(lista_excede_promedio)):
             if(lista_excede_promedio[j][1] < menor) and not(j in taken2):
                 indicador = j
                 menor = lista_excede_promedio[j][1]
                 valor = lista_excede_promedio[j]
-
     taken2.append(indicador)
     lista_reordenada.append(valor)
     rango -= 1
@@ -86,21 +86,20 @@ peso = 0
 beneficio = 0
 aceptados = 0
 contador = 0
-
 for i in range(len(lista_reordenada)):
     if(peso <= capacidad):
         contador += 1
         peso += lista_reordenada[i][1]
     else:
         break
-
 contador -= 1
 peso = 0
 for i in range(contador):
     peso += lista_reordenada[i][1]
     beneficio += lista_reordenada[i][2]
-
 print("Los objetos en la mochila son: \n")
 for i in range(contador):
     print(lista_reordenada[i][0] + " con un peso de objeto de " + str(lista_reordenada[i][1]) + " y con un beneficio de " + str(lista_reordenada[i][2]))
 print("\n Con un peso en la mochila de " + str(peso) + " y un beneficio maximo de " + str(beneficio))
+tiempo = time.clock() - start_time
+print ("Tiempo de ejecución : " + str(tiempo))
